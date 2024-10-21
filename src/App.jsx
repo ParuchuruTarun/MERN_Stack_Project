6737aaf5ccd1a4a59baa461e965490a97e2e185c
Paruchuru Tarun <paruchurutarun111@gmail.com>
2024-10-21 21:35:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Menu_bar from './Menu_bar'
+import Prompt_Area from './Prompt_area'
+
+
+function App() {
+
+  return(
+    <div className="Page">
+      <Menu_bar></Menu_bar>
+      <Prompt_Area></Prompt_Area>
+    </div>
+  )
+}
+
+export default App
